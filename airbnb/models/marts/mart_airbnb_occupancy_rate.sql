e25743603c67bacb319555c7943de7444d2fe7b3
Marcelo de Oliveira <marcelo_rde@yahoo.com.br>
2024-11-07 11:29:01 +0000

    New analysis

---
+++ airbnb/models/marts/mart_airbnb_occupancy_rate.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='table') }}
+
+WITH calendar_data AS (
+    SELECT 
+        listing_id,
+        EXTRACT(YEAR FROM listing_date_avaliable) AS year,
+        EXTRACT(MONTH FROM listing_date_avaliable) AS month,
+        listing_avaliable
+    FROM {{ ref('int_airbnb_calendar') }}
+)
+
+, monthly_occupancy AS (
+    SELECT
+        year,
+        month,
+        listing_id,
+        COUNT(*) AS total_days, 
+        SUM(CASE WHEN listing_avaliable = FALSE THEN 1 ELSE 0 END) AS occupied_days  -- conta apenas os dias ocupados
+    FROM calendar_data
+    GROUP BY year, month, listing_id
+)
+
+SELECT
+    year,
+    month,
+    AVG(CAST(occupied_days AS FLOAT) / total_days) * 100 AS avg_occupancy_rate  -- calcula a taxa média de ocupação
+FROM monthly_occupancy
+GROUP BY year, month
+ORDER BY year, month;
